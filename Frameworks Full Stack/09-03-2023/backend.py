059d70e104ddb13d34266fc1d7a54117c998aab2
Pedro Egydio <pedroe01@hotmail.com>
2023-03-15 20:14:34 -0300

    Change style & add gitignore

--- Frameworks Full Stack/09-03-2023/backend.py
+++ Frameworks Full Stack/09-03-2023/backend.py
@@ -6,18 +6,20 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/<param1>/<param2>')
-def params_page(param1, param2):
-    text = f'Os parâmetros enviados foram {param1} e {param2} e a soma deles é {int(param1) + int(param2)}'
-    return text
+# @app.route('/<param1>/<param2>')
+# def params_page(param1, param2):
+#     text = f'Os parâmetros enviados foram {param1} e {param2} e a soma deles é {int(param1) + int(param2)}'
+#     return text
 
 @app.route('/media')
 def media():
     n1 = request.args.get('n1')
     n2 = request.args.get('n2')
     resultado = ''
+    media = ''
     if n1 and n2:
         media = (float(n1) + float(n2)) / 2
+        if media > 10: media = 10
         if media >= 7.0:
             resultado = f'Aluno aprovado | Média: {media}'
         elif media > 4 and media < 7:
@@ -25,7 +27,7 @@ def media():
         else:
             resultado = f'Aluno reprovado | Média: {media}'
 
-    return render_template('media.html', resultado=resultado)
+    return render_template('media.html', resultado=resultado, media=media)
 
 if __name__ == '__main__':
     app.run(debug=True)
